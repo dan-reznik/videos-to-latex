8ca1fa66b0501388064a499b270d0d682aa00c27
Dan Reznik <dreznik@gmail.com>
2022-05-31 12:49:57 -0300

    polished $X_{nn}$

--- utils.R
+++ utils.R
@@ -4,12 +4,21 @@ clean_video <- function(str) {
     str_replace_all(fixed('_'),'\\_')
 }
 
+clean_title <- function(str) {
+  str %>%
+    str_replace_all(fixed("&"),"\\&") %>%
+    str_replace_all("X\\((\\d+?)\\)","$X_{\\1}$") %>%
+    str_replace_all("X(\\d+)","$X_{\\1}$")
+  # str_replace_all("N=(\\d+?)","$N=\\1$")
+}
+
 make_bibtex_entry <- function(df) {
-  df_str <- df %>% mutate(str=glue("\\item \\textit{{{Title}}}, {Year}. youtu.be/\\href{{{Video1}}}{{\\nolinkurl{{{clean_video(Video1)}}}}}"))
+  df_str <- df %>% mutate(str=glue("\\item \\textit{{{clean_title(Title)}}}, {Year}. youtu.be/\\href{{{Video1}}}{{\\nolinkurl{{{clean_video(Video1)}}}}}"))
   df_coll <- df_str$str %>% str_c(collapse="\n")
   glue("\\begin{{enumerate}}[resume]\n{df_coll}\n\\end{{enumerate}}\n")
 }
 
 make_bibtex <- function(df,key) {
-  glue("\\section{{{key} ({nrow(df)})}\n\n{make_bibtex_entry(df)}\n\n")
+  glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_bibtex_entry(df)}\n\n")
 }
+
