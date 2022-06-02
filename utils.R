3370333013abe2e8e6d999dde8ae08d73e3edbf9
Dan Reznik <dreznik@gmail.com>
2022-06-02 11:55:55 -0300

    added thumbs and descriptions

--- utils.R
+++ utils.R
@@ -31,7 +31,7 @@ clean_title <- function(str) {
 make_enum_entry_long <- function(df) {
   df_str <- df %>%
     # mutate(str=glue("\\item \\textit{{{clean_title(Title)}}}, {Year}. \\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}"))
-    mutate(str=glue("\\item \\textbf{{{clean_title(Title)}}}, {Year}. ",
+    mutate(str=str_glue("\\item \\textbf{{{clean_title(Title)}}}, {Year}. ",
                     "\\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}} ",
                     "{descr}\n"))
   df_coll <- df_str$str %>% str_c(collapse="\n")
@@ -41,17 +41,38 @@ make_enum_entry_long <- function(df) {
 make_enum_entry <- function(df) {
   df_str <- df %>%
     # mutate(str=glue("\\item \\textit{{{clean_title(Title)}}}, {Year}. \\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}"))
-    mutate(str=glue("\\item \\textbf{{{clean_title(Title)}}}, {Year}. ",
+    mutate(str=str_glue("\\item \\textbf{{{clean_title(Title)}}} ({duration}), {month(date)}/{year(date)}. ",
                     "\\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}"))
   df_coll <- df_str$str %>% str_c(collapse="\n")
-  glue("\\begin{{enumerate}}[resume]\n{df_coll}\n\\end{{enumerate}}\n")
+  str_glue("\\begin{{enumerate}}[resume]\n{df_coll}\n\\end{{enumerate}}\n")
 }
+
+make_center_video <- function(fname) {
+  str_glue("\n\n\\begin{{center}}",
+           "\\includegraphics[width=.5\\textwidth]{{{fname}}}",
+           "\\end{{center}}\n")
+}
+
+make_enum_entry_video <- function(df) {
+  df_str <- df %>%
+    # mutate(str=glue("\\item \\textit{{{clean_title(Title)}}}, {Year}. \\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}"))
+    mutate(
+      video_fname = str_glue("pics/{video_id}.jpg"),
+      str=str_glue("\\item \\textbf{{{clean_title(Title)}}} ",
+                   "({duration}), {month(date)}/{year(date)}. ",
+                   "\\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}",
+                   "{make_center_video(video_fname)}\n",
+                   "% \\input{{descr/{sprintf('%03d',id)}_{video_id}}}"))
+  df_coll <- df_str$str %>% str_c(collapse="\n")
+  str_glue("\\begin{{enumerate}}[resume]\n{df_coll}\n\\end{{enumerate}}\n")
+}
+
 
 make_subsec_entry_long <- function(df) {
   df_str <- df %>%
     # mutate(str=glue("\\item \\textit{{{clean_title(Title)}}}, {Year}. \\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}"))
-    mutate(str=glue("\\subsection{{{clean_title(Title)}}}\n",
-                    "Year: {Year}. N: {N}. ",
+    mutate(str=str_glue("\n\\subsection{{{clean_title(Title)}}}\n",
+                    "Duration: {duration}. Published: {month(date)}/{year(date)}. N: {N}. ",
                     "\\href{{{Video1}}}{{\\url{{{clean_video(Video1)}}}}}\n\n",
                     "{clean_title(descr)}\n"))
   df_coll <- df_str$str %>% str_c(collapse="\n")
@@ -59,10 +80,14 @@ make_subsec_entry_long <- function(df) {
 }
 
 make_enum <- function(df,key) {
-  glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_enum_entry(df)}\n\n")
+  str_glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_enum_entry(df)}\n\n")
+}
+
+make_enum_video <- function(df,key) {
+  str_glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_enum_entry_video(df)}\n\n")
 }
 
 make_subsec <- function(df,key) {
-  glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_subsec_entry_long(df)}\n\n")
+  str_glue("\\section{{{clean_title(key)} ({nrow(df)})}\n\n{make_subsec_entry_long(df)}\n\n")
 }
 
--- utils_youtube.R
+++ utils_youtube.R
@@ -1,4 +1,5 @@
 library(httr)
+library(stringr)
 
 # see: https://www.yuichiotsuka.com/youtube-data-extract-r/
 key <- "AIzaSyCeBljlBtzlUB8oPYs4t9Y8GOhgCdJ12LA"
@@ -6,16 +7,53 @@ channel <- "UCJ3BPNdm0z1UzcJYTwxlsjA"
 user <- "dreznik"
 base <- "https://www.googleapis.com/youtube/v3/"
 
+# https://youtu.be/wiknzClcm4s
+
+fmt_dur_str <- function(dur) {
+  str_replace(dur,"^PT(\\d+?)M(\\d+?)S$","\\1m\\2s")
+}
+# "PT2M25S" => (2,25)
+extr_dur_m_s <- function(dur) {
+  m_s <- str_replace(dur,"^PT(\\d+?)M(\\d+?)S$","\\1,\\2")
+  m_s_l <- str_split_fixed(m_s,",",2) %>% map_int(as.integer)
+  m_s_l
+}
+
+extr_dur_mins <- function(dur) {
+  m_s_l <- extr_dur_m_s(dur)
+  m_s_l[[1]] + m_s_l[[2]]/60
+}
+
 get_video_descr <- function(video) {
   api_params <- 
     paste(paste0("key=", key), 
           paste0("id=", video), 
-          "part=snippet",
+          # fileDetails: must be connected to my account
+          "part=snippet,contentDetails",
           sep = "&")
   api_call <- paste0(base, "videos?", api_params)
   api_result <- GET(api_call)
   json_result <- content(api_result, "text", encoding="UTF-8")
   channel.json <- fromJSON(json_result, flatten = T)
   # channel.df <- as.data.frame(channel.json)
-  channel.json$items$snippet.description
+  thumb_url <- channel.json$items$snippet.thumbnails.high.url
+  descr <- channel.json$items$snippet.description
+  date <- channel.json$items$snippet.publishedAt
+  # PT<int>M<int>S
+  duration <- channel.json$items$contentDetails.duration
+  list(thumb_url=thumb_url,descr=descr,date=as.Date(date),duration=fmt_dur_str(duration))
+}
+
+get_videos_descr <- function(df,fname) {
+  df_descr <- df %>%
+    #head(10) %>%
+    #slice(300:390) %>%
+    mutate(video_id=str_remove_all(Video1,"http[s?]://youtu.be/")) %>%
+    mutate(descr_url=video_id %>% map_dfr(get_video_descr)) %>%
+    mutate(descr=descr_url$descr,
+           thumb_url=descr_url$thumb_url,
+           date=descr_url$date,
+           duration=descr_url$duration)
+  df_descr %>% write_rds("fname","bz2")
+  df_descr
 }
